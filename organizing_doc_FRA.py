c1e8ab107320e19880a0c38bc55b6ed96de93cd6
Chansong <dlacksthd94@naver.com>
2024-11-19 09:48:42 -0800

    zip QA docs

--- organizing_doc_FRA.py
+++ organizing_doc_FRA.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import zipfile
 import pandas as pd
 
 DATA_FOLDER = 'data/'
@@ -29,3 +30,21 @@ for i, (series, subseries, file_name) in df_FRA_doc[['series', 'subseries', 'fil
     file_path_to = BIGDATA_FOLDER + DOC_FRA_FOLDER + series + '/' + subseries + '/' + file_name
     if os.path.exists(file_path_from):
         os.rename(file_path_from, file_path_to)
+
+######################### find Q&A files ###########################
+if not os.path.exists(BIGDATA_FOLDER + 'QA/'):
+    os.mkdir(BIGDATA_FOLDER + 'QA/')
+
+bool_qa = df_FRA_doc['file_name'].fillna('').str.match(r'.*((?<!CE)QA|Q&A|QnA|QNA|FAQ|question|Qs and As|qa|q&a|qna|faq|qs and as|qsas).*')
+for i, row in df_FRA_doc[bool_qa].iterrows():
+    series, subseries, file_name = row[['series', 'subseries', 'file_name']]
+    qa_file_path_from = BIGDATA_FOLDER + DOC_FRA_FOLDER + series + '/' + subseries + '/' + file_name
+    if os.path.exists(qa_file_path_from):
+        1
+        qa_file_path_to = BIGDATA_FOLDER + 'QA/' + file_name
+        shutil.copyfile(qa_file_path_from, qa_file_path_to)
+
+# Create a zip file
+qa_zip_name = BIGDATA_FOLDER + 'QA'
+qa_folder_path = BIGDATA_FOLDER + 'QA/'
+shutil.make_archive(qa_zip_name, 'zip', qa_folder_path)
