ac6b5da461b1a713c2b593328f601b5aa13ca4e3
dlacksthd94 <dlacksthd94@naver.com>
2025-04-28 17:51:53 -0700

    Edit: news filter

--- filter_news.py
+++ filter_news.py
@@ -6,37 +6,37 @@ pd.set_option('display.max_rows', 100)
 DATA_FOLDER = 'data/'
 FN_DF = 'df_news.csv'
 FN_DF_FILTER = 'df_news_filter.csv'
+COLUMNS_CONTENT = ['np_url', 'tf_url', 'rd_url', 'gs_url', 'np_html', 'tf_html', 'rd_html', 'gs_html']
 
 path_df = DATA_FOLDER + FN_DF
+path_df_filter = DATA_FOLDER + FN_DF_FILTER
+
 df = pd.read_csv(path_df, parse_dates=['pub_date'])
 
-######### drop na
-df = df.dropna(subset=['url'])
-df = df.dropna(subset=['content'])
-
-######### drop duplicates
-df = df.drop_duplicates('url')
-
-
-######### drop human verification articles
-human_verification = [
-    'Press & Hold to confirm you are\n\na human',
-    'This website is using a security service to protect itself from online attacks.',
-    'Verify you are human by completing the action below',
-    "Sorry, we have to make sure you're a human before we can show you this page."
+######### drop non-article
+no_article_patterns = [
+    '^\s+$',
+    'Press & Hold to confirm you',
+    'You are not authorized to access',
+    'Edge: Too Many Requests',
+    '403 Forbidden',
+    'These cookies are necessary for our services to function',
+    'Selling, Sharing, Targeted Advertising',
+    'Please enable JS and disable any ad blocker',
+    'We may use personal information to support',
+    'Access to this site has been denied.',
+    'This website is using a security service to protect itself from online attacks',
+    'we provide online advertising services that use cookies and similar technologies to collect information',
+    "We won't sell or share your personal information to inform the ads you see.",
+    'we process personal information to inform which ads you see on our services',
 ]
-pattern = r'|'.join(human_verification)
-df = df[~df['content'].str.contains(pattern, na=False)]
+pattern = r'|'.join(no_article_patterns)
+df[COLUMNS_CONTENT] = df[COLUMNS_CONTENT].apply(lambda col: col.replace(to_replace=pattern, value=float('nan'), regex=True))
 
-######### drop content not containing query1
-path_json_list_keywords = DATA_FOLDER + 'dict_list_keywords.json'
-with open(path_json_list_keywords, "r") as file:
-    dict_list_keywords = json.load(file)
-list_query1 = dict_list_keywords['list_query1']
-pattern = '|'.join(list_query1)
-contain_query1 = df['content'].str.contains(pattern, case=False, na=False) | df['title'].str.contains(pattern, case=False, na=False)
-df = df[contain_query1]
+######### drop na
+df = df.dropna(subset=['news_id'])
+df = df.dropna(subset=COLUMNS_CONTENT, how='all')
 
+######### save
 df = df.reset_index(drop=True)
-path_df_filter = DATA_FOLDER + FN_DF_FILTER
 df.to_csv(path_df_filter, index=False)
