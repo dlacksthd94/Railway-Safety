7a86eb33466aff4fae37bc972c50b54fdd2a8a4e
dlacksthd94 <dlacksthd94@naver.com>
2025-05-02 11:19:40 -0700

    feat: change inference function to `transformers.pipeline`

--- label_news_test.py
+++ label_news_test.py
@@ -1,12 +1,11 @@
 import torch
 import pandas as pd
-from transformers import AutoTokenizer, AutoModelForCausalLM
 import numpy as np
 from tqdm import tqdm
 import re
 import time
 import gc
-import utils_inference
+from transformers import pipeline
 
 DATA_FOLDER = 'data/'
 FN_DF_SAMPLE = 'df_news_label_sample.csv'
@@ -18,71 +17,65 @@ df_label = pd.read_csv(path_df_label)
 
 N_SIM = 5
 list_column = ['model', 'time', 'queryA', 'queryB', 'queryC']
-dict_model = {
+dict_models = {
     # 'google/gemma-3-1b-it',
-    # 'google/gemma-3-1b-pt',
+    # 'google/gemma-3-1b-pt': {},
     # 'google/txgemma-2b-predict',
     # 'google/txgemma-9b-predict',
-    # 'google/txgemma-9b-chat',
-    # 'Qwen/Qwen2.5-Omni-7B', # transformers version failure
-    'Qwen/Qwen2.5-7B-Instruct-1M': {
-        'max_new_tokens': 1,
-        'temperature': 0.7,
-        'top_p': 0.8,
-        'do_sample': True,
-        # 'do_sample': False,
-        'repetition_penalty': 1.05,
-    }, # best performance
-    'microsoft/Phi-4-mini-instruct': {
-        'max_new_tokens': 1,
-        # 'temperature': 0.0,
-        'do_sample': False, # no need to set 'temperature' to 0
-    }, # second best performance
-    'nvidia/Llama-3.1-Nemotron-Nano-8B-v1': {
-        'max_new_tokens': 1,
-        'temperature': 0.6,
-        'top_p': 0.95,
-        'do_sample': True,
-        # 'do_sample': False,
-    }, # good 
-    "deepseek-ai/DeepSeek-R1-Distill-Llama-8B": {
-        'max_new_tokens': 1,
-        'temperature': 0.6,
-        'top_p': 0.95,
-        'do_sample': True,
-        # 'do_sample': False,
-    }, # good
-    # "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B", # low performance
-    # "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", # too low performance
+    # 'google/txgemma-9b-chat': {},
+    # 'Qwen/Qwen2.5-Omni-7B': {}, # transformers version failure
+    'Qwen/Qwen2.5-7B-Instruct-1M': {}, # third best performance
+    'microsoft/Phi-4-mini-instruct': {}, # best performance
+    'nvidia/Llama-3.1-Nemotron-Nano-8B-v1': {}, # good but sometimes bad
+    "deepseek-ai/DeepSeek-R1-Distill-Llama-8B": {}, # second best performance
+    "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B": {}, # low performance
+    "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B": {}, # too low performance
+    "facebook/bart-large-mnli": {'candidate_labels': ["YES", "NO"]} # worst performance
 }
 
 df = pd.DataFrame(columns=list_column)
-for model_path, config in tqdm(dict_model.items()):
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForCausalLM.from_pretrained(model_path)
-    _ = model.to(DEVICE)
+for model_path, config in tqdm(dict_models.items()):
+    pipe = pipeline(model=model_path, device_map=DEVICE, **config)
+    task = pipe.task
+    if task == 'text-generation' and pipe.generation_config.pad_token_id is None:
+        pipe.generation_config.pad_token_id = pipe.tokenizer.eos_token_id
+    if config.get('temperature', None) == 0 or config.get('do_sample', None) == False:
+        n_sim = 1
+    else:
+        n_sim = N_SIM
+
+    answer_constraint = 'Answer only YES or NO.'
+    if task == 'zero-shot-classification':
+        answer_constraint = ''
 
     list_prec = []
     start = time.perf_counter()
     for i, row in tqdm(df_label.iterrows(), total=df_label.shape[0], leave=False):
         _, _, content, label = row
-        list_prec_temp = []
+        list_prec_by_query = []
         list_query = [
-            # "Is this article reporting a train accident? Answer with YES or NO.",
-            # "Is this article's main topic a train accident? Answer with YES or NO.",
-            "Is this article's main topic reporting a train accident? Answer only YES or NO.", # best
-            "Is the primary focus of this article to report a train accident? Answer only YES or NO.", # good but sometimes bad
-            "Does the article report on a train accident as its main topic? Answer only YES or NO.", # not bad
+            f"Is this article's main topic reporting a train accident? {answer_constraint}", # best
+            f"Is the primary focus of this article to report a train accident? {answer_constraint}", # good but sometimes bad
+            f"Does the article report on a train accident as its main topic? {answer_constraint}", # not bad
         ]
+        
         for query in list_query:
-            prompt = f"context: {content}\n\nquery:{query}\n\nanswer:"
-            if model.config.pad_token_id is None:
-                config['pad_token_id'] = tokenizer.eos_token_id
-            list_answer = [utils_inference.inference(tokenizer, model, prompt, config, DEVICE) for _ in range(N_SIM)]
+            prompt = f"Context: {content}\n\nQuestion: {query}\n\nAnswer:"
+
+            list_answer = []
+            if task == 'zero-shot-classification':
+                output = pipe(prompt)
+                answer = output['labels'][np.argmax(output['scores'])]
+                list_answer.append(answer)
+            elif task == 'text-generation':
+                for _ in range(n_sim):
+                    output = pipe(prompt, max_new_tokens=1)
+                    answer = output[0]['generated_text'].split('Answer:')[-1].strip()
+                    list_answer.append(answer)
             list_answer_binary = [1 if re.search(r'yes', answer, re.IGNORECASE) else 0 for answer in list_answer]
-            prec_temp = (np.array(list_answer_binary) == (label == 'YES')).mean()
-            list_prec_temp.append(prec_temp)
-        list_prec.append(list_prec_temp)
+            prec = (np.array(list_answer_binary) == (label == 'YES')).mean()
+            list_prec_by_query.append(prec)
+        list_prec.append(list_prec_by_query)
     end = time.perf_counter()
     elapsed_time = end - start
 
@@ -91,7 +84,7 @@ for model_path, config in tqdm(dict_model.items()):
     row = [model_path, elapsed_time] + prec_mean
     df.loc[len(df)] = row
 
-    del model
+    del pipe
     gc.collect()
     torch.cuda.empty_cache()
 
