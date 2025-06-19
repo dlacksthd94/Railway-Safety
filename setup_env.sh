a1f3a01ed99c339700079c80b2b0b88233d5d15d
dlacksthd94 <dlacksthd94@naver.com>
2025-06-19 14:03:17 -0700

    feat: transcribe and convert the original PDF/img doc form to JSON format

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+#################### create miniconda env
 ENV_NAME="rw_safety"
 
 set -e
@@ -14,27 +15,29 @@ conda create -n $ENV_NAME python=3.10 -y
 
 conda activate $ENV_NAME
 
+#################### install packages
 pip cache purge
 conda clean --all -y
 
 pip install pandas
-pip install feedparser
-pip install selenium
-pip install requests
-pip install newspaper3k lxml_html_clean trafilatura readability-lxml goose3
+pip install feedparser requests # for Google RSS
+pip install selenium newspaper3k lxml_html_clean trafilatura readability-lxml goose3 # for scraping news articles
 pip install streamlit
-pip install transformers
+pip install openai pytesseract # for transcribing report form in json format
+pip install transformers accelerate # for labeling news & populating the report form
+pip install sentencepiece # for running VLMs from Hugging Face
 
+#################### install chrome driver for news scraping
 find . -name "chrome-*" -exec rm -rf {} +
 find . -name "chromedriver-*" -exec rm -rf {} +
 # LATEST VERSION HERE!!! https://googlechromelabs.github.io/chrome-for-testing/
-VERSION="135.0.7049.95"
+VERSION="136.0.7103.94"
 OS="$(uname -s)"
 ARCH="$(uname -m)"
 case "${OS}" in
   Linux)
     PLATFORM="linux64"
-    DOWNLOADER="wget -q --show-progress -O"
+    DOWNLOADER="wget -q --show-progress"
     ;;
   Darwin)
     if [[ "${ARCH}" = "arm64" ]]; then
@@ -55,3 +58,35 @@ URL="https://storage.googleapis.com/chrome-for-testing-public/${VERSION}/${PLATF
 eval "${DOWNLOADER} ${URL}"
 unzip "chrome-${PLATFORM}.zip"
 unzip "chromedriver-${PLATFORM}.zip"
+
+# #################### install Google Cloud CLI (optional)
+# find . -name "google-cloud*" -exec rm -rf {} +
+# # install			https://cloud.google.com/sdk/docs/install
+# # authentification	https://cloud.google.com/docs/authentication/set-up-adc-local-dev-environment
+# wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+# tar -xf google-cloud-cli-linux-x86_64.tar.gz
+# ./google-cloud-sdk/install.sh
+# ./google-cloud-sdk/bin/gcloud init
+# pip install google-cloud-documentai google-cloud-vision
+
+# #################### install AWS Textract (optional)
+# find . -name "aws*" -exec rm -rf {} +
+# refer to:			https://docs.aws.amazon.com/textract/latest/dg/analyzing-document-text.html?utm_source=chatgpt.com
+# wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -O awscliv2.zip
+# unzip awscliv2.zip
+# AWS_DIR="$PWD/aws"
+# mkdir "$AWS_DIR/bin"
+# ./aws/install -i $AWS_DIR -b "$AWS_DIR/bin"
+# if grep -Fxq "export PATH=\"$AWS_DIR/bin:\$PATH\"" ~/.bashrc; then
+#   echo "✅ Line already present in ~/.bashrc"
+# else
+#   echo "export PATH=\"$AWS_DIR/bin:\$PATH\"" >> ~/.bashrc
+# fi
+# pip install boto3 pdf2image
+# aws configure --profile textract
+
+# #################### isntall Azure Form Recognizer (optional)
+# https://learn.microsoft.com/en-us/azure/ai-services/document-intelligence/concept/choose-model-feature?view=doc-intel-4.0.0#pretrained-document-analysis-models
+# pip install azure-ai-formrecognizer
+# pip install azure-identity
+# pip install azure-ai-documentintelligence
