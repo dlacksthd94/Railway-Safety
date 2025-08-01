6b90ce6cd6905dc20c2cc31ee5f73dcc6a1eb507
dlacksthd94 <dlacksthd94@naver.com>
2025-07-31 22:27:26 -0700

    feat(metric): calculate accuracy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 import argparse
 import pandas as pd
+import utils_scrape
+import json5
+import numpy as np
+import tqdm
 
 parser = argparse.ArgumentParser(
     description="Run main.py"
@@ -13,50 +17,54 @@ parser = argparse.ArgumentParser(
 # Optional (flag-style) arguments
 parser.add_argument(
     "--c_api",
-    # choices=["fast", "safe"],
-    # default="safe",
-    # help="Processing mode (default: safe)"
+    choices=['Huggingface', 'OpenAI', 'None'],
+    default="Huggingface",
+    help="API to use for processing"
 )
 
 parser.add_argument(
     "--c_model",
     type=str,
-    # default=0.5,
-    # help="Confidence threshold (default: 0.5)"
+    choices=['Qwen/Qwen2.5-VL-7B-Instruct', 'OpenGVLab/InternVL3-8B-hf', 'ByteDance-Seed/UI-TARS-1.5-7B', 'None'],
+    default='Qwen/Qwen2.5-VL-7B-Instruct',
+    help="Model to use for processing"
 )
 
 parser.add_argument(
     "--c_n_generate",
-    type=int
+    type=int,
+    default=4,
     # action="store_true",
-    # help="Enable verbose output"
+    help="Number of generations"
 )
 
-parser.add_argument("--c_json_source")
+parser.add_argument(
+    "--c_json_source",
+    type=str,
+    choices=['csv', 'pdf', 'img'],
+    default='img',
+    help="Source of JSON data"
+)
 
 # parser.add_argument("--r_question_batch")
 
 args = parser.parse_args()
 
 
-############ config
+############### config
 from config import Config, ConversionConfig, RetrievalConfig
 
 ### ConversionConfig
-# config_conversion = ConversionConfig(api=None, model=None, n_generate=None, json_source='csv')
-# config_conversion = ConversionConfig(api='OpenAI', model='o4-mini', n_generate=8, json_source='pdf')
-config_conversion = ConversionConfig(api='Huggingface', model='Qwen/Qwen2.5-VL-7B-Instruct', n_generate=4, json_source='img')
-# config_conversion = ConversionConfig(api='Huggingface', model='OpenGVLab/InternVL3-8B-hf', n_generate=4, json_source='img')
-# config_conversion = ConversionConfig(api=args.c_api, model=args.c_model, n_generate=args.c_n_generate, json_source=args.c_json_source)
+config_conversion = ConversionConfig(api=args.c_api, model=args.c_model, n_generate=args.c_n_generate, json_source=args.c_json_source)
 
 ### RetrievalConfig
-config_retrieval = RetrievalConfig(api='Huggingface', model='microsoft/Phi-4-mini-instruct', n_generate=1, question_batch='single')
-# config_retrieval = RetrievalConfig(api='Huggingface', model='microsoft/Phi-4-mini-instruct', n_generate=1, question_batch='group')
+# config_retrieval = RetrievalConfig(api='Huggingface', model='microsoft/Phi-4-mini-instruct', n_generate=1, question_batch='single')
+config_retrieval = RetrievalConfig(api='Huggingface', model='microsoft/Phi-4-mini-instruct', n_generate=1, question_batch='group')
 config = Config(conversion=config_conversion, retrieval=config_retrieval)
 
 print('------------Configuration DONE!!------------')
 
-############ paths
+############### paths
 from utils import make_dir
 
 DIR_DATA_ROOT = 'data'
@@ -75,7 +83,8 @@ FN_FORM57_JSON_GROUP = f'form57_group.json'
 
 FN_DF_FORM57_RETRIEVAL = f'df_form57_retrieval.csv'
 
-FN_DF_MATCH = 'df_match.csv'
+FN_DF_TEST_SET = 'df_test_set.csv'
+FN_DICT_COL_IDX_NAME = 'dict_col_idx_name.jsonc'
 
 conversion_model_replaced = config.conversion.model.replace('/', '@')
 retrieval_model_replaced = config.retrieval.model.replace('/', '@')
@@ -93,11 +102,12 @@ path_form57_img = os.path.join(DIR_DATA_ROOT, FN_FORM57_IMG)
 path_form57_json = os.path.join(path_dir_config_json, FN_FORM57_JSON)
 path_form57_json_group = os.path.join(path_dir_config_json, FN_FORM57_JSON_GROUP)
 path_df_form57_retrieval = os.path.join(path_dir_config_result, FN_DF_FORM57_RETRIEVAL)
-path_df_match = os.path.join(DIR_DATA_ROOT, 'df_match.csv')
+path_df_test_set = os.path.join(DIR_DATA_ROOT, FN_DF_TEST_SET)
+path_dict_col_idx_name = os.path.join(DIR_DATA_ROOT, FN_DICT_COL_IDX_NAME)
 
 print('------------Setting path DONE!!------------')
 
-############ convert to json
+############### convert to json
 from convert_report_to_json import csv_to_json, pdf_to_json, img_to_json
 
 if config.conversion.json_source == 'csv':
@@ -109,18 +119,34 @@ elif config.conversion.json_source == 'img':
 
 print('------------Conversion DONE!!------------')
 
-############ extract keywords
+############### extract keywords
 from extract_keywords import extract_keywords
 
 df_retrieval = extract_keywords(path_form57_json, path_form57_json_group, path_df_form57_retrieval, path_df_news_label, config.retrieval)
 
 print('------------Retrieval DONE!!------------')
 
-############ match news and csv file using retrieved information
-assert os.path.exists(path_df_match)
-df_match = pd.read_csv(path_df_match)
-df_match = df_match[df_match['match'] == 1]
+############### made hand-annotated samples (ONLY ONE-TIME TASK)
+assert os.path.exists(path_df_test_set)
+df_test_set = pd.read_csv(path_df_test_set)
+df_test_set = df_test_set[df_test_set['match'] == 1]
+
+df_test_set = df_test_set[['incident_id', 'news_id']]
+assert df_test_set['news_id'].is_unique, '==========Warning: News is not unique!!!==========='
+
+############### match news and csv file
+df_match = df_retrieval.merge(
+    df_test_set,
+    on=['incident_id','news_id'],
+    how='inner'
+)
 
 print('------------Matching DONE!!------------')
 
-############ calculate the accuracy
+############### calculate the accuracy
+from utils import get_acc_table
+
+# list_answer_type_selected = ['choice', 'digit', 'str', 'list', 'etc']
+list_answer_type_selected = ['choice']
+df_acc = get_acc_table(path_form57_csv, path_dict_col_idx_name, df_match, dict_form57, list_answer_type_selected)
+df_acc.loc[:, '1':].dropna(axis=1, how='all').mean().mean()
--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 import os
 import time
+import utils_scrape
+import json5
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
 
 def make_dir(path):
     if not os.path.exists(path):
@@ -23,3 +28,79 @@ class Timer:
         m = int((total_seconds % 3600) // 60)
         s = total_seconds % 60               # still a float now
         return f"{h:02d}:{m:02d}:{s:06.3f}"   # e.g. 00:01:02.357
+
+def get_acc_table(path_form57_csv, path_dict_col_idx_name, df_match, dict_form57, list_answer_type_selected):
+    df_form57_csv = pd.read_csv(path_form57_csv)
+    df_form57_csv = df_form57_csv[df_form57_csv['State Name'] == 'CALIFORNIA']
+    df_form57_csv['hash_id'] = df_form57_csv.apply(utils_scrape.hash_row, axis=1)
+    df_form57_csv['Date'] = pd.to_datetime(df_form57_csv['Date'])
+    df_form57_csv = df_form57_csv[df_form57_csv['Date'] >= '2000-01-01']
+
+    with open(path_dict_col_idx_name, 'r') as f:
+        dict_col_idx_name = json5.load(f)
+
+    dict_idx_answer_type = {
+        'digit': {"14": "Estimated Vehicle Speed", "18": "Railroad Car Unit Position", "21": "Temperature",
+                  "28": "Number of Locomotive Units", "29": "Number of Cars", "38": "User Age", "47": "Vehicle Damage Cost", "48": "Number Vehicle Occupants", "50": "Number People On Train"},
+        'str': {"1": "Railroad Name", "2": "Other Railroad Name", "3": "Maintenance Railroad Name", "7": "Nearest Station", "9": "County Name", "11": "City Name", "26": "Track Name", "27": "Track Class"},
+        'list': {"5": ["Date", "Month", "Day", "Incident Year"], "6": ["Time", "Hour", "Minute", "AM/PM"], "12": ["Highway Name", "Public/Private"],
+                 "20c": ["Hazmat Released Name", "Hazmat Released Quantity", "Hazmat Released Measure"], "30": ["Train Speed", "Estimated/Recorded Speed"],
+                 "46": ["Crossing Users Killed", "Crossing Users Injured"], "49": ["Employees Killed", "Employees Injured"], "52": ["Passengers Killed", "Passengers Injured"]},
+        'choice': {"13": "Highway User Code", "15": "Vehicle Direction Code", "16": "Highway User Position Code",
+                   "17": "Equipment Involved Code", "19": "Equipment Struck Code",
+                   "20a": "Hazmat Involvement Code", "20b": "Hazmat Released by Code",
+                   "22": "Visibility Code", "23": "Weather Condition Code",
+                   "24": "Equipment Type Code", "25": "Track Type Code", "31": "Train Direction Code",
+                   "34": "Roadway Condition Code", "35": "Crossing Warning Location Code", "36": "Warning Connected To Signal", "37": "Crossing Illuminated",
+                   "39": "User Sex", "40": "User Struck By Second Train", "41": "Highway User Action Code", "42": "Driver Passed Vehicle", "43": "View Obstruction Code",
+                   "44": "Driver Condition Code", "45": "Driver In Vehicle"},
+        'etc': {"10": "State Name"}
+    }
+
+    # choose only choice-answer idx
+    dict_idx_selected = {k: v for answer_type_selected in list_answer_type_selected for k, v in dict_idx_answer_type[answer_type_selected].items()}
+
+    df_acc = df_match.copy(deep=True)
+    df_acc = df_acc.drop('match', axis=1, errors='ignore')
+    df_acc.loc[:, '1':] = np.nan
+    for idx_match, row_match in tqdm(df_match.iterrows(), total=df_match.shape[0]):
+        incident_id = row_match['incident_id']
+        row_csv = df_form57_csv[df_form57_csv['hash_id'] == incident_id]
+        list_score_temp = []
+        for col_idx, col_name in dict_idx_selected.items():
+            retrieval = row_match[col_idx]
+            label = row_csv[col_name]
+            if isinstance(retrieval, str):
+                retrieval = retrieval.strip('\'"').strip()
+            acc_temp = np.nan
+            if not (isinstance(retrieval, str) and retrieval.lower() == 'unknown') and pd.notna(retrieval) and label.notna().squeeze():
+                if col_idx in dict_idx_answer_type['str']:
+                    # must use LLM to check non-choice answer
+                    pass
+                elif col_idx in dict_idx_answer_type['list']:
+                    # must use a fine-grained metric
+                    pass
+                elif col_idx in dict_idx_answer_type['digit']:
+                    pass
+                    acc_temp = (retrieval == label.squeeze())
+                elif col_idx in dict_idx_answer_type['choice']:
+                    choices = dict_form57[col_idx]['choices']
+                    retrieval_choie_key = retrieval.upper() if isinstance(retrieval, str) else str(int(retrieval)).upper()
+                    retrieval_choice_value = choices.get(retrieval_choie_key, '').upper()
+                    label_choice_key = label.squeeze().upper() if isinstance(label.squeeze(), str) else str(int(label.squeeze()))
+                    label_choice_value = choices.get(label_choice_key, '').upper()
+                    
+                    acc_temp_by_key_key = (retrieval_choie_key == label_choice_key)
+                    acc_temp_by_key_value = (retrieval_choie_key == label_choice_value)
+                    acc_temp_by_value_key = (retrieval_choice_value == label_choice_key)
+                    acc_temp = any([acc_temp_by_key_key, acc_temp_by_key_value, acc_temp_by_value_key])
+                elif col_idx in dict_idx_answer_type['etc']:
+                    pass
+                else:
+                    raise f"no col like {col_idx}: {col_name}"
+            list_score_temp.append(acc_temp)
+            print(f'{acc_temp}\t{retrieval}=={label.squeeze()}\t{col_name}')
+        # print(row_match['rd_url'])
+        print('--------------------------------------------------')
+        df_acc.loc[idx_match, list(dict_idx_selected.keys())] = list_score_temp
+    return df_acc
